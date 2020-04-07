4c7b1455bd8fb824f4e9f994f59c70202f70d542
Eran Markus <eran132@gmail.com>
2020-04-08 02:15:26 +0300

    Adding mutually exclusive

--- grepython4.py
+++ grepython4.py
@@ -8,6 +8,12 @@ if __name__ == '__main__':
     parser.add_argument('-f', '--files', nargs='*', help="files to search in, stdin is "
                                                          "used if not file specified",
                         default='-')
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('-u', '--underscore', action='store_true', help='prints "^" under the '
+                                                                       'matching text')
+    group.add_argument('-c', '--color', action='store_true', help="highlight matching text")
+    group.add_argument('-m', '--machine', action='store_true', help='generate machine readable '
+                                                                    'output')
     args = parser.parse_args()
     pattern = re.compile(args.regex)
 
